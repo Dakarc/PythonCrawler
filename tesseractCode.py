b8fb35c8acbc687ac209a390cbaf7513822b1117
8598749 <8598749@qq.com>
2019-06-03 07:56:03 +0800

    scrapy start

--- tesseractCode.py
+++ tesseractCode.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\TesseractOCR\tesseract.exe'
 
-image = Image.open("1.png")
+image = Image.open("2.png")
 
-text = pytesseract.image_to_string(image)
+text = pytesseract.image_to_string(image,lang='chi_sim')
 
 print(text)
